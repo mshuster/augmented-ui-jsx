73d4a33b720e3845715718e5a774a7f6b5956cff
Mark Shuster <shusterm@twitch.tv>
2020-07-23 13:58:30 -0700

    restructure folders, update ignores, adds fields to package.json

--- .storybook/src/index.stories.tsx
+++ .storybook/src/index.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Augmented } from "./augmented";
+import { Augmented } from "../../src/augmented";
 import "reset-css";
 import "augmented-ui/augmented.css";
 import "./styles.css";
